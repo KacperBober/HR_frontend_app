765964c29904d4996f01ab0a492a03b2ed5ff635
BlazejR <blazejrajkowski@poczta.fm>
2022-11-10 12:35:44 +0100

    added list-employee

--- app/employeedata.js
+++ app/employeedata.js
@@ -1,8 +1,14 @@
+
+const util = require ('util');
 const axios = require("axios");
 
-exports.addEmployee = async (newEmployee) => {
-  axios
-    .post("http://localhost:8080/api/employee", newEmployee)
-    .then((res) => console.log(res))
-    .catch((err) => console.log(err));
-};
+
+exports.getEmployees = async () => { 
+  try {  
+      const employeeResponse = await axios.get('http://localhost:8080/api/employees')
+      //console.log(employeeResponse.data)
+      return employeeResponse.data
+    } catch (e) {
+       return new Error('Could not get employee')
+    }
+}
