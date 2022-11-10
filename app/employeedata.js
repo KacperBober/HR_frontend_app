f2d61e1114a660dc8c246ba5f9494aed8624f3a9
Tomasz Łaski <tomasz.laski0001@gmail.com>
2022-11-10 13:04:20 +0100

    add again POST request

--- app/employeedata.js
+++ app/employeedata.js
@@ -1,14 +1,21 @@
-
-const util = require ('util');
+const util = require("util");
 const axios = require("axios");
 
+exports.addEmployee = async (newEmployee) => {
+  axios
+    .post("http://localhost:8080/api/employee", newEmployee)
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err));
+};
 
-exports.getEmployees = async () => { 
-  try {  
-      const employeeResponse = await axios.get('http://localhost:8080/api/employees')
-      //console.log(employeeResponse.data)
-      return employeeResponse.data
-    } catch (e) {
-       return new Error('Could not get employee')
-    }
-}
+exports.getEmployees = async () => {
+  try {
+    const employeeResponse = await axios.get(
+      "http://localhost:8080/api/employees"
+    );
+    //console.log(employeeResponse.data)
+    return employeeResponse.data;
+  } catch (e) {
+    return new Error("Could not get employee");
+  }
+};
