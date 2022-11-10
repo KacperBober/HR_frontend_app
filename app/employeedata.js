5ccbf2d44878db977e398c778c795d8b39a73b65
Kacper Bober <kacper.bober@kainos.com>
2022-11-10 13:55:37 +0100

    merge test

--- app/employeedata.js
+++ app/employeedata.js
@@ -1,11 +1,23 @@
 const util = require("util");
 const axios = require("axios");
 
-exports.addEmployee = async (newEmployee) => {
+exports.addEmployee = async (newee) => {
+  axios
+    .post("http://localhost:8080/api/employee", newee)
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err));
+};
+
+exports.addEmployee2 = async (newEmployee) => {
   axios
     .post("http://localhost:8080/api/employee", newEmployee)
     .then((res) => console.log(res))
     .catch((err) => console.log(err));
 };
 
-
+exports.addEmployee2 = async (newEmployee) => {
+  axios
+    .post("http://localhost:8080/api/employee", newEmployee)
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err));
+};
