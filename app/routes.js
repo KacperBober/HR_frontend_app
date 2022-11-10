5d28478403d75561dcb271d23acfbb6eb398a3b6
Tomasz Łaski <tomasz.laski0001@gmail.com>
2022-11-10 12:01:33 +0100

    add form with post method

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,16 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
+const employeedata = require("./employeedata");
 
 // Add your routes here - above the module.exports line
 
-module.exports = router
+router.get("/addemployee", async (req, res) => {
+  res.render("new-employee-form");
+});
+
+router.post("/addemployee", async (req, res) => {
+  var employee = req.body;
+  let insertedKey = await employeedata.addEmployee(req.body);
+});
+
+module.exports = router;
