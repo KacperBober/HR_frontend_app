ca8a1f076cba0f07e02b3615be7888153eb44bb5
Kacper Bober <kacper.bober@kainos.com>
2022-11-10 13:51:26 +0100

    deleted function from file

--- app/employeedata.js
+++ app/employeedata.js
@@ -8,14 +8,4 @@ exports.addEmployee = async (newEmployee) => {
     .catch((err) => console.log(err));
 };
 
-exports.getEmployees = async () => {
-  try {
-    const employeeResponse = await axios.get(
-      "http://localhost:8080/api/employee"
-    );
-    //console.log(employeeResponse.data)
-    return employeeResponse.data;
-  } catch (e) {
-    return new Error("Could not get employee");
-  }
-};
+
